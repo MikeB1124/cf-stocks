301828d34303fe8efec9546c359133df642d13ee
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-06-19 14:43:03 -0700

    shared stack deploy

---
+++ shared.py
@@ -0,0 +1,24 @@
+from stacker.blueprints.base import Blueprint
+from troposphere import (
+    Output,
+    Ref,
+    s3,
+)
+
+
+class Stocks(Blueprint):
+    VARIABLES = {"env-dict": {"type": dict}}
+
+    def create_template(self):
+        s3_bucket = s3.Bucket(
+            "StockS3Bucket",
+            BucketName=self.get_variables()["env-dict"]["BucketName"],
+        )
+        self.template.add_resource(s3_bucket)
+
+        self.template.add_output(
+            Output(
+                "BucketName",
+                Value=Ref(s3_bucket),
+            )
+        )
