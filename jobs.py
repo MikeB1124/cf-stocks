6e8c971fa056e3030a66d8e2da58e1c855401b18
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-06-22 11:47:58 -0700

    adding profit calculator lambda and scheduler

--- jobs.py
+++ jobs.py
@@ -240,9 +240,230 @@ class Stocks(Blueprint):
         )
         self.template.add_resource(order_sync_scheduler)
 
+    def create_stock_profit_calculator_lambda(self):
+        lambda_role = self.template.add_resource(
+            iam.Role(
+                "ProfitCalculatorLambdaExecutionRole",
+                AssumeRolePolicyDocument={
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Principal": {
+                                "Service": [
+                                    "lambda.amazonaws.com",
+                                    "apigateway.amazonaws.com",
+                                ]
+                            },
+                            "Action": ["sts:AssumeRole"],
+                        }
+                    ],
+                },
+                Policies=[
+                    iam.Policy(
+                        PolicyName="ProfitCalculatorLambdaS3Policy",
+                        PolicyDocument={
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": ["s3:GetObject"],
+                                    "Resource": [
+                                        Sub(
+                                            "arn:aws:s3:::${BucketName}/*",
+                                            BucketName=self.get_variables()["env-dict"][
+                                                "BucketName"
+                                            ],
+                                        )
+                                    ],
+                                }
+                            ],
+                        },
+                    ),
+                    iam.Policy(
+                        PolicyName="ProfitCalculatorLambdaLogPolicy",
+                        PolicyDocument={
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": "logs:CreateLogGroup",
+                                    "Resource": Sub(
+                                        "arn:aws:logs:${AWS::Region}:${AWS::AccountId}:*"
+                                    ),
+                                },
+                                {
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "logs:CreateLogStream",
+                                        "logs:PutLogEvents",
+                                    ],
+                                    "Resource": [
+                                        Sub(
+                                            "arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/lambda/${LambdaName}:*",
+                                            LambdaName=self.get_variables()["env-dict"][
+                                                "ProfitCalculatorLambdaName"
+                                            ],
+                                        )
+                                    ],
+                                },
+                            ],
+                        },
+                    ),
+                    iam.Policy(
+                        PolicyName="ProfitCalculatorLambdaSecretsManagerPolicy",
+                        PolicyDocument={
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": ["secretsmanager:GetSecretValue"],
+                                    "Resource": [
+                                        Sub(
+                                            "arn:aws:secretsmanager:${AWS::Region}:${AWS::AccountId}:secret:${SecretId}-5BII06",
+                                            SecretId=self.get_variables()["env-dict"][
+                                                "SharedSecretsId"
+                                            ],
+                                        )
+                                    ],
+                                }
+                            ],
+                        },
+                    ),
+                ],
+            )
+        )
+
+        self.stocks_profit_calculator_lambda_function = awslambda.Function(
+            "ProfitCalculatorLambdaFunction",
+            FunctionName=self.get_variables()["env-dict"]["ProfitCalculatorLambdaName"],
+            Code=awslambda.Code(
+                S3Bucket=Ref(self.existing_stocks_bucket),
+                S3Key=Sub(
+                    "lambdas/${LambdaName}.zip",
+                    LambdaName=self.get_variables()["env-dict"][
+                        "ProfitCalculatorLambdaName"
+                    ],
+                ),
+            ),
+            Environment=awslambda.Environment(
+                Variables={
+                    "SHARED_SECRETS": self.get_variables()["env-dict"][
+                        "SharedSecretsId"
+                    ]
+                }
+            ),
+            Handler="handler",
+            Runtime="provided.al2023",
+            Role=GetAtt(lambda_role, "Arn"),
+        )
+        self.template.add_resource(self.stocks_profit_calculator_lambda_function)
+
+        self.profit_calculator_api_resource = apigateway.Resource(
+            "ProfitCalculatorResource",
+            ParentId="{{resolve:ssm:/stocks/sync/resource/id}}",
+            RestApiId="{{resolve:ssm:/stocks/api/id}}",
+            PathPart="profit",
+        )
+        self.template.add_resource(self.profit_calculator_api_resource)
+
+        profit_calculator_api_method = apigateway.Method(
+            "ProfitCalculatorMethod",
+            DependsOn=self.stocks_profit_calculator_lambda_function,
+            AuthorizationType="NONE",
+            ApiKeyRequired=True,
+            HttpMethod="POST",
+            RestApiId="{{resolve:ssm:/stocks/api/id}}",
+            ResourceId=Ref(self.profit_calculator_api_resource),
+            Integration=apigateway.Integration(
+                IntegrationHttpMethod="POST",
+                Type="AWS_PROXY",
+                Uri=Sub(
+                    "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/${LambdaArn}/invocations",
+                    LambdaArn=GetAtt(self.stocks_profit_calculator_lambda_function, "Arn"),
+                ),
+            ),
+        )
+        self.template.add_resource(profit_calculator_api_method)
+
+        self.template.add_resource(
+            awslambda.Permission(
+                "ProfitCalculatorInvokePermission",
+                DependsOn=self.stocks_profit_calculator_lambda_function,
+                Action="lambda:InvokeFunction",
+                FunctionName=self.get_variables()["env-dict"][
+                    "ProfitCalculatorLambdaName"
+                ],
+                Principal="apigateway.amazonaws.com",
+                SourceArn=Sub(
+                    "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${ApiId}/*/POST/sync/profit",
+                    ApiId="{{resolve:ssm:/stocks/api/id}}",
+                ),
+            )
+        )
+    
+    def create_profit_calculator_scheduler(self):
+        scheduler_execution_role = self.template.add_resource(
+            iam.Role(
+                "ProfitCalculatorSchedulerExecutionRole",
+                AssumeRolePolicyDocument={
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Effect": "Allow",
+                                "Principal": {
+                                    "Service": "scheduler.amazonaws.com"
+                                },
+                                "Action": "sts:AssumeRole",
+                            }
+                        ]
+                },
+                Policies=[
+                    iam.Policy(
+                        PolicyName="ProfitCalculatorSchedulerExecutionPolicy",
+                        PolicyDocument={
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": ["lambda:InvokeFunction"],
+                                    "Resource": "*",
+                                },
+                            ],
+                        },
+                    )
+                ]
+            )
+        )
+
+        profit_calculator_scheduler = scheduler.Schedule(
+            "ProfitCalculatorScheduler",
+            Name="profit-calculator-scheduler",
+            Description="Profit Calculator Scheduler",
+            ScheduleExpression="cron(0/10 * ? * MON-FRI *)",
+            ScheduleExpressionTimezone="America/Los_Angeles",
+            FlexibleTimeWindow=scheduler.FlexibleTimeWindow(
+                Mode="OFF"
+            ),
+            Target=scheduler.Target(
+                Arn=GetAtt(self.stocks_profit_calculator_lambda_function, "Arn"),
+                Input='{"httpMethod": "POST", "path": "/sync/profit"}',
+                RetryPolicy=scheduler.RetryPolicy(
+                    MaximumEventAgeInSeconds=86400,
+                    MaximumRetryAttempts=185
+                ),
+                RoleArn=GetAtt(scheduler_execution_role, "Arn")
+            )
+        )
+        self.template.add_resource(profit_calculator_scheduler)
+
+
+
 
     def create_template(self):
         self.get_existing_stocks_bucket()
         self.create_stocks_order_sync_lambda()
         self.create_order_sync_scheduler()
+        self.create_stock_profit_calculator_lambda()
+        self.create_profit_calculator_scheduler()
         return self.template
